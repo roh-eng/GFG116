91dbd602fd48ada1a3e11fa05b5554bfb1e731ba
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2026-01-31 14:11:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Value equal to index value/value-equal-to-index-value.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+
+class Solution {
+    public List<Integer> valueEqualToIndex(List<Integer> nums) {
+        // code here
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for(int i=0;i<nums.size();i++){
+            if(nums.get(i)==i+1){
+                arr.add(i+1);
+            }
+        }
+        return arr;
+    }
+}
