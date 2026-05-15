896210f5b12b7ff0bd9f2ae7df23a05c4b24fbd3
DEVADULA ROHIT <23P31A4466@acet.ac.in>
2026-05-15 16:57:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search in a Matrix/search-in-a-matrix.java
+++ Difficulty: Easy/Search in a Matrix/search-in-a-matrix.java
@@ -1,7 +1,4 @@
-// User function Template for Java
-
 class Solution {
-    // Function to search a given integer in a matrix.
     public boolean searchMatrix(int[][] mat, int x) {
         // code here
         for(int i=0;i<mat.length;i++){
